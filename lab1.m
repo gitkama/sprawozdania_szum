9e3b96c78d20b0423f8589a652894cc7b99f169f
Kamil <kamkzarow@gmail.com>
2018-11-12 08:54:49 -0800

    Add files via upload

---
+++ lab1.m
@@ -0,0 +1,14 @@
+% Na poczatku nalezy odpalic ROS'a, stworzyc symulacje zolwia (sterowanie
+% oraz wizualizacje), dzieki czemu powstanie topic. Nastepnie mozna dzialac
+% w matlab
+
+ros init;
+
+while(1)
+    turtle = rossubscriber('/turtle1/pose');
+    move_info = receive(turtle);
+    x = move_info.X;
+    y = move_info.Y;
+    plot(x,y,'x')
+    axis([0 12 0 12])
+end
